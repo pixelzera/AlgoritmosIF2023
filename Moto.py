7d584887df531dc03bcb1b988b2204719af7a159
pixelzera <120063882+pixelzera@users.noreply.github.com>
2023-01-25 20:28:23 -0300

    Create Moto.py

---
+++ Moto.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+
+@dataclass
+class Moto:
+    placa: str
+    marca: str
+    modelo: str
+    cor: str
+    ano: int
+
+  from dataclasses import dataclass
+#Sistema de Cadastro de Motos
+
+@dataclass
+class moto:
+    placa: str
+    marca: str
+    modelo: str
+    cor: str
+    ano: int
+
+
+def main():
+    pass
+
+class moto(Cadastro):
+    def __init__(self):
+        self.placa = ()
+        self.marca = ()
+        self.modelo = ()
+        self.menu() = ()
+
+def list():
+    pass
+
+def delete():
+    pass
+
+def update():
+    pass
+
+main():
