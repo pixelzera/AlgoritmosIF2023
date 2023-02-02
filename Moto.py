6f9847e73e004eb3ade37a74365e309126af66ed
pixelzera <120063882+pixelzera@users.noreply.github.com>
2023-02-02 19:21:49 -0300

    Update Moto.py

--- Moto.py
+++ Moto.py
@@ -1,14 +1,8 @@
+from ast import Num
+from audioop import add
 from dataclasses import dataclass
-
-@dataclass
-class Moto:
-    placa: str
-    marca: str
-    modelo: str
-    cor: str
-    ano: int
-
-  from dataclasses import dataclass
+from optparse import Option
+from tkinter import Menu
 #Sistema de Cadastro de Motos
 
 @dataclass
@@ -22,13 +16,36 @@ class moto:
 
 def main():
     pass
+    menu = ''' 
+    [1]Cadastro
+    [2]Editar
+    [3]Consultar
+    [4]Sair
+    '''
+print (menu)
+op = int(input("Selecione a OPÇÃO desejada : "))  
 
-class moto(Cadastro):
-    def __init__(self):
-        self.placa = ()
-        self.marca = ()
-        self.modelo = ()
-        self.menu() = ()
+    while (op !=5):
+                  
+        if op == 1:
+            add()
+        if op == 2:
+                update()
+        if op == 3:
+                list()
+        if op == 4:
+                delete()
+        if op == 5:
+                exit()
+ 
+      
+                     
+                    
+def add():
+    pass
+
+def update():
+    pass
 
 def list():
     pass
@@ -36,7 +53,4 @@ def list():
 def delete():
     pass
 
-def update():
-    pass
-
-main():
+main()
